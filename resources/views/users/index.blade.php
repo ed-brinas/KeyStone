6255ebc0b9304d1ad8652d4266b68a7e1a4475e8
Edward Brinas <edwardbrinas@gmail.com>
2025-10-10 23:33:08 +0300

    Bug fixes and code updates on 10-Oct

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -296,7 +296,7 @@
 
 
 <!-- MODIFIED START - 2025-10-10 23:18 - Replaced local asset call for Bootstrap JS with CDN to fix modal issues. -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" xintegrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
 <!-- MODIFIED END - 2025-10-10 23:18 -->
 
 
