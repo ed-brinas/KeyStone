4e10112b4dc3783207ce95d8131f0eb498c6ef43
Edward Brinas <edwardbrinas@gmail.com>
2025-10-11 10:52:52 +0300

    Bug fixes and code updates on 11-Oct

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+
 // Redirect the root URL to the users index page.
 Route::get('/', function () {
     return redirect()->route('users.index');
 });
 
-// User Management Routes
-Route::resource('users', UserController::class)->only(['index', 'store', 'update']);
+// User management routes
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
+Route::put('/users/{guid}', [UserController::class, 'update'])->name('users.update');
+Route::put('/users/{guid}', [UserController::class, 'update'])->name('users.update');
 Route::post('/users/{guid}/reset-password', [UserController::class, 'resetPassword'])->name('users.resetPassword');
+
